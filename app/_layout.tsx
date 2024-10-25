ab9de5c9a28203da847bf91fedbc90bc464dbafc
H. Rian <rian@advanced.tw>
2024-10-25 22:02:13 +0800

    unistyle basic example

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -28,10 +28,11 @@ export default function RootLayout() {
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
+      <Stack >
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="+not-found" />
       </Stack>
     </ThemeProvider>
   );
 }
+
